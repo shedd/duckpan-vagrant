6f745f8426f4dcecc8e4b5c78a17638e9f40a996
Vasanth Elavarasan <vasanthela2@gmail.com>
2013-11-16 01:29:33 -0600

    Remove cookbooks other than duckpan

--- cookbooks/duckpan/recipes/default.rb
+++ cookbooks/duckpan/recipes/default.rb
@@ -6,6 +6,6 @@
 include_recipe 'perl'
 include_recipe 'git'
 
-bash 'download and install duckpan' do
-   code ::File.open('/vagrant/duckpan.sh').read
-end
+#bash 'download and install duckpan' do
+#   code ::File.open('/vagrant/duckpan.sh').read
+#end

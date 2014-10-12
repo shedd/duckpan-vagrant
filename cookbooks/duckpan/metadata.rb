98bd91c768b0fdd8a6e15b2e481f679984eba6af
Mike DePaulo <mikedep333@gmail.com>
2014-10-12 15:03:11 -0400

    Specify cookbook license as "Apache v2.0" rather than "Apache"

--- cookbooks/duckpan/metadata.rb
+++ cookbooks/duckpan/metadata.rb
@@ -1,7 +1,7 @@
 name             'duckpan'
 maintainer       'Larry Gilbert'
 maintainer_email 'larry@L2G.to'
-license          'Apache'
+license          'Apache v2.0'
 description      'Installs/Configures duckpan'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
